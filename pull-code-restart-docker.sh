f26e4d2e729994fe57d0b376b3b364c271ea54c5
FindHappyLee <libbyyounh@outlool.com>
2025-09-10 18:57:12 +0800

    feat: edit sh

--- pull-code-restart-docker.sh
+++ pull-code-restart-docker.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-docker-compose down
-
 # 拉取最新代码
 if git pull; then
     echo "Git pull succeeded, restarting Docker containers..."
+    docker-compose down
     # 重启Docker容器
     sh ./docker-compose-build.sh
 else
